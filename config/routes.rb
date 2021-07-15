3699805574b1f5749db56a067bfa8a385a13f594
s-b-edu <sbaur@chicagobooth.edu>
2021-07-15 00:22:05 +0000

    aquarius

--- config/routes.rb
+++ config/routes.rb
@@ -25,8 +25,8 @@ Rails.application.routes.draw do
   get("/zodiacs/capricorn", { :controller => "earth", :action => "goat" })
   
   get("/zodiacs/gemini", { :controller => "air", :action => "twins" })
-  # get(" /zodiacs/libra", { :controller => "air", :action => "scales" })
-  # get ("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
+  get("/zodiacs/libra", { :controller => "air", :action => "scales" })
+  get("/zodiacs/aquarius", { :controller => "air", :action => "waterbearer" })
   
   # get("/zodiacs/cancer " { :controller => "earth" :action => "crab" })
   # get("/zodiac/ scorpio", { :controller => "Water", :action => "scorpion" })
